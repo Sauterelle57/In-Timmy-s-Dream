9cc4a54d62ac8db6dd552a994994d3d45cdaafbe
jujulegeek <julian.lambert@epitech.eu>
2022-04-20 05:24:24 -0400

    forest collisions beginning

--- src/forest/init_elem_forest.c
+++ src/forest/init_elem_forest.c
@@ -16,5 +16,15 @@ body_t *init_forest_elem(sfRenderWindow *window, int nb_elem)
     1398}, (sfVector2f){0, 0});
     elem[1] = create_body("map/swamp3d.png", (sfIntRect){1010, 600, 2364,
     1398}, (sfVector2f){0, 0});
+    elem[2] = create_body("other/blue_block.png", (sfIntRect){0, 0, 2500, 290},
+    (sfVector2f){-670, 590});
+    elem[3] = create_body("other/blue_block.png", (sfIntRect){0, 0, 420, 100},
+    (sfVector2f){-670, -350});
+    elem[4] = create_body("other/blue_block.png", (sfIntRect){0, 0, 50, 1000},
+    (sfVector2f){-720, -350});
+    elem[5] = create_body("other/blue_block.png", (sfIntRect){0, 0, 50, 300},
+    (sfVector2f){50, -350});
+    elem[6] = create_body("other/blue_block.png", (sfIntRect){0, 0, 420, 100},
+    (sfVector2f){-670, -350});
     return (elem);
 }
