d28264e00c894867d4e29d54a807ccfb6af47157
Baldazzi <tristan.baldazzi@epitech.eu>
2022-04-26 15:13:57 +0200

    Add: interest in combat scene

--- src/combat/main_combat.c
+++ src/combat/main_combat.c
@@ -15,11 +15,12 @@ scene_t init_combat(game_t *g)
     body_t *elem = create_maze(elem, g);
     int nb_elem = 378;
     sfMusic *scene_music = sfMusic_createFromFile("music/back.ogg");
-    int nb_interest = 2;
+    int nb_interest = 3;
     interest_t *interest = init_combat_interest(nb_interest, elem);
     scene_t combat = {0, buttons, nb_button, elem, nb_elem, interest,
     nb_interest, scene_music};
 
+    combat.interest = interest;
     maze_generation(23, 15);
     combat.draw = &draw_combat;
     combat.event = &event_combat;
@@ -27,24 +28,3 @@ scene_t init_combat(game_t *g)
     sfMusic_setLoop(combat.scene_music, sfTrue);
     return (combat);
 }
-
-/*scene_t init_combat(game_t *g)
-{
-    button_t *buttons = init_combat_button(g->window);
-    int nb_button = 0;
-    body_t *elem = create_maze(elem);
-    int nb_elem = 377;
-    interest_t *interest = init_combat_interest(g->window);
-    int nb_interest = 0;
-    scene_t combat = {buttons, nb_button, elem, nb_elem, interest,
-    nb_interest};
-
-    maze_generation(23, 15);
-    //elem[376] = create_body("other/font_invisible.png", (sfIntRect){0, 0, 4032, 3024},
-    //(sfVector2f){-1700, -1300});
-    combat.draw = &draw_combat;
-    combat.event = &event_combat;
-    combat.anim = &anim_combat;
-    return (combat);
-}
-*/
